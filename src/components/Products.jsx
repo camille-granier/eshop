c8286043141df95f6ff96b1fe5faa24bfe5578c6
camille-granier <granierc@protonmail.com>
2021-12-19 15:30:13 +0100

    adding product card

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Products = () => {
 
@@ -29,18 +29,49 @@ const Products = () => {
         return (
             <>Loading ...</>
         )
-    }
+    };
+
+    const ShowProducts = () => {
+        return (
+            <>
+                <div className='buttons d-flex justify-content-center mb-5 pb-5'>
+                    <button className='btn btn-outline-dark me-2'>All</button>
+                    <button className='btn btn-outline-dark me-2'>Men's Clothing</button>
+                    <button className='btn btn-outline-dark me-2'>Women's Clothing</button>
+                    <button className='btn btn-outline-dark me-2'>Jewellery</button>
+                    <button className='btn btn-outline-dark me-2'>Electronic</button>
+                </div>
+                {filter.map((product) => {
+                    return (
+                        <><div className='col-md-3'>
+                            <div class="card">
+                                <img class="card-img-top" src={product.image} alt={product.title}/>
+                                    <div class="card-body">
+                                        <h5 class="card-title">{product.title}</h5>
+                                        <p class="card-text">€{product.price}</p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                            </div>
+                        </div>
+                        </>
+                    )
+                })
+                }
+            </>
+        );
+    };
+
     return (
         <div>
             <div className='container my-5 py-5'>
                 <div className='row'>
                     <div className='col-12 mb-5'>
-                        <h1 className='display-6 fw-bolder text-center'>Latest products</h1>
+                        <h1 className='display-6 fw-bolder text-center'>Latest Products</h1>
                         <hr />
                     </div>
                 </div>
                 <div className='row justify-content-center'>
-                    {loading ? <Loading/> : <ShowProducts/>}
+                    {loading ? <Loading /> : <ShowProducts />}
                 </div>
             </div>
         </div>
