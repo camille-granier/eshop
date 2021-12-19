1a4542c5d6127c92dd41578cb90dc1151a05750c
camille-granier <granierc@protonmail.com>
2021-12-19 14:50:01 +0100

    initialise Products comp

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import Products from './Products';
 
 const Home = () => {
     return (
         <div className='hero'>
-            <div class="card bg-dark text-white border-0">
-                <img src="assets/bg.jpg" class="card-img" alt="Background" height="590vh"/>
-                <div class="card-img-overlay d-flex flex-column justify-content-center">
+            <div className="card bg-dark text-white border-0">
+                <img src="assets/bg.jpg" className="card-img" alt="Background" height="550vh"/>
+                <div className="card-img-overlay d-flex flex-column justify-content-center">
                     <div className='container'></div>
-                    <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
-                    <p class="card-text lead fs-2">Check out all the trends</p>
+                    <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+                    <p className="card-text lead fs-2">Check out all the trends</p>
                 </div>
             </div>
-
+            <Products />
         </div>
     );
 };
