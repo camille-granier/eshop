0c9d929cf0f2fcf13d7af5f7424d82b8f9ed26dd
camille-granier <granierc@protonmail.com>
2021-12-20 12:13:38 +0100

    styling card and setting button

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Skeleton from 'react-loading-skeleton';
 
 const Products = () => {
 
@@ -27,7 +28,20 @@ const Products = () => {
 
     const Loading = () => {
         return (
-            <>Loading ...</>
+            <>
+                <div className='col-md-3'>
+                    <Skeleton height={350}/>
+                </div>
+                <div className='col-md-3'>
+                    <Skeleton height={350}/>
+                </div>
+                <div className='col-md-3'>
+                    <Skeleton height={350}/>
+                </div>
+                <div className='col-md-3'>
+                    <Skeleton height={350}/>
+                </div>
+            </>
         )
     };
 
@@ -35,22 +49,22 @@ const Products = () => {
         return (
             <>
                 <div className='buttons d-flex justify-content-center mb-5 pb-5'>
-                    <button className='btn btn-outline-dark me-2'>All</button>
-                    <button className='btn btn-outline-dark me-2'>Men's Clothing</button>
-                    <button className='btn btn-outline-dark me-2'>Women's Clothing</button>
-                    <button className='btn btn-outline-dark me-2'>Jewellery</button>
-                    <button className='btn btn-outline-dark me-2'>Electronic</button>
+                    <button className='btn btn-outline-dark me-2' onClick={()=>setFilter(data)}>All</button>
+                    <button className='btn btn-outline-dark me-2' onClick={()=>filterProducts("men's clothing")}>Men's Clothing</button>
+                    <button className='btn btn-outline-dark me-2' onClick={()=>filterProducts("women's clothing")}>Women's Clothing</button>
+                    <button className='btn btn-outline-dark me-2' onClick={()=>filterProducts("jewellery")}>Jewellery</button>
+                    <button className='btn btn-outline-dark me-2' onClick={()=>filterProducts("electronic")}>Electronic</button>
                 </div>
                 {filter.map((product) => {
                     return (
-                        <><div className='col-md-3'>
-                            <div class="card">
-                                <img class="card-img-top" src={product.image} alt={product.title}/>
-                                    <div class="card-body">
-                                        <h5 class="card-title">{product.title}</h5>
-                                        <p class="card-text">€{product.price}</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                                    </div>
+                        <><div className='col-md-3 mb-4'>
+                            <div class="card h-100 text-center p-4" key={product.id}>
+                                <img class="card-img-top" height="250px" src={product.image} alt={product.title} />
+                                <div class="card-body">
+                                    <h5 class="card-title mb-0">{product.title.substring(0, 12)}...</h5>
+                                    <p class="card-text lead fw-bold">€{product.price}</p>
+                                    <a href="#" class="btn btn-outline-dark">Buy Now</a>
+                                </div>
                             </div>
                         </div>
                         </>
