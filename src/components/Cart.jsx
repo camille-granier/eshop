1fce35cd802c4ca4512e2de8889d46e6c242bfd1
camille-granier <granierc@protonmail.com>
2021-12-24 15:53:18 +0100

    cart

---
+++ src/components/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addCart, delCart } from '../redux/actions';
+
+const Cart = () => {
+
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Cart;
